d3f9833b354cb18d9ff38d77cd346afde4410b94
Isabelo Castillo <isabelocastillosanchez@gmail.com>
2023-12-11 23:40:22 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import db
+from sqlalchemy import Column, Integer, String, Boolean
+
+
+class Tarea(db.Base):
+    __tablename__='tarea'
+    __table_args__ = {'extend_existing': True}  # Añade esta línea
+    id = Column(Integer, primary_key=True)
+    contenido = Column(String(100), nullable=False)
+    hecha = Column(Boolean)
+
+
+    def __init__(self, contenido, hecha):
+        self.contenido = contenido
+        self.hecha = hecha
+
+
+    def __repr__(self):
